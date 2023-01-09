7d5284ee57fa945672240bf7700486509064e6a7
Fe1ker <93716685+Fe1ker@users.noreply.github.com>
2023-01-09 10:08:07 -0300

    inicializando projeto

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Agendador</title>
+</head>
+<body>
+    <header>
+        <h1>Sistema Agendador 1.0</h1>
+        <nav>
+            <a href="index.php?menuop = home">Home</a>  | 
+            <a href="index.php?menuop = contato">Contato</a>  | 
+            <a href="index.php?menuop = tarefas">Tarefas</a>  | 
+            <a href="index.php?menuop = eventos">Eventos</a>  
+        </nav>
+    </header>
+    <main>
+        <?php
+        switch("menuop"){
+            case 1:
+        }
+        ?>
+    </main>
+</body>
+</html>
